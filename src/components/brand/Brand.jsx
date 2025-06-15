215b5a8dc8df54b293cbb6ba61ee263ccc5ae0a0
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-15 18:04:11 +0900

    Add Brand component with styling for responsive layout

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -3,7 +3,17 @@ import "./Brand.css";
 import { google, atlassian, dropbox, slack, shopify } from "./imports";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+        <img src={slack} alt="slack" />
+        <img src={atlassian} alt="atlassian" />
+        <img src={dropbox} alt="dropbox" />
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
